4c465b082d9943a7abdb96708729b1af1cb9838d
fkm-X3 <fkmbloodshead@gmail.com>
2026-02-02 15:05:11 +1100

    Removed unneeded files

--- packager/main.cpp
+++
@@ -1,8 +0,0 @@
-// Minimal C++ launcher that calls into the Rust browser entry point.
-// Build this after running `cargo build --release` so the Rust static library exists.
-
-extern "C" int rusty_web_main();
-
-int main() {
-    return rusty_web_main();
-}
